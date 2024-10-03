cded19f098e02a74487981ebf7cb8f8f1a5b2010
Sashank Balusu <sashankbalusu@gmail.com>
2024-10-02 22:08:03 -0700

    Make design changes to match figma

--- app/page.tsx
+++ app/page.tsx
@@ -8,41 +8,67 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.content}>
-        <ProfileIcon size={24} />
-        <p>rbeggs</p>
-        <p>September 19</p>
-        <p>
-          In response to the growing homelessness crisis in San Francisco, a
-          local nonprofit organization, Code Tenderloin, has launched a
-          comprehensive initiative aimed at providing long-term solutions for
-          individuals experiencing homelessness. The organization, founded in
-          2015, is dedicated to addressing both immediate needs and underlying
-          causes of homelessness through a combination of shelter services, job
-          training programs, and mental health support. Read more online:
-          https://www.codetenderloin.org/
-        </p>
+        <div className={styles.header}>
+          <img src="../assets/images/bp-logo.png"></img>
+          <h1>blueprint &nbsp;</h1>
+          <h1>blog</h1>
+        </div>
+        <div className={styles.post}>
+          <div className={styles.headingInfo}>
+            <ProfileIcon size={24} />
+            <p>rbeggs</p>
+            <p>September 19</p>
+          </div>
+          <p className={styles.descriptionText}>
+            In response to the growing homelessness crisis in San Francisco, a
+            local nonprofit organization, Code Tenderloin, has launched a
+            comprehensive initiative aimed at providing long-term solutions for
+            individuals experiencing homelessness. The organization, founded in
+            2015, is dedicated to addressing both immediate needs and underlying
+            causes of homelessness through a combination of shelter services,
+            job training programs, and mental health support. Read more online:
+            <a style={{ color: 'blue' }} href="https://www.codetenderloin.org">
+              https://www.codetenderloin.org/
+            </a>
+          </p>
+          <img
+            style={{
+              width: '-webkit-fill-available',
+              borderRadius: '8px',
+              marginTop: '2%',
+            }}
+            src="https://cdn.britannica.com/51/178051-050-3B786A55/San-Francisco.jpg"
+          ></img>
+          <div className={styles.postInteractions}>
+            <HeartIcon size={24} />
+            <p>256 Likes</p>
+            <ShareIcon size={24} />
+          </div>
+        </div>
 
-        <p>
-          Image Link:
-          https://cdn.britannica.com/51/178051-050-3B786A55/San-Francisco.jpg
-        </p>
+        <hr style={{ marginTop: '4%', marginBottom: '4%' }}></hr>
+        <div>
+          <div className={styles.headingInfo}>
+            <ProfileIcon size={24} />
+            <p>daviddd</p>
+            <p>September 20</p>
+          </div>
+          <p className={styles.descriptionText + ' ' + styles.comment}>
+            This organization is doing amazing work tackling the complex root
+            causes of the issue.
+          </p>
+        </div>
+        <div>
+          <div className={styles.headingInfo}>
+            <ProfileIcon size={24} />
+            <p>vppraggie</p>
+            <p>September 21</p>
+          </div>
 
-        <HeartIcon size={24} />
-        <p>256 Likes</p>
-        <ShareIcon size={24} />
-
-        <ProfileIcon size={24} />
-        <p>daviddd</p>
-        <p>September 20</p>
-        <p>
-          This organization is doing amazing work tackling the complex root
-          causes of the issue.
-        </p>
-
-        <ProfileIcon size={24} />
-        <p>vppraggie</p>
-        <p>September 21</p>
-        <p>Thanks for sharing!</p>
+          <p className={styles.descriptionText + ' ' + styles.comment}>
+            Thanks for sharing!
+          </p>
+        </div>
       </div>
     </main>
   );
